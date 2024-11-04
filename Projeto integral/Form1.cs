c242638326cfd28eaf443255a08b37574e48b93a
danilospinola <102962780+danilospinola@users.noreply.github.com>
2024-11-04 20:45:46 -0300

    Inicio da Plotagem de gráfico

---
+++ Projeto integral/Form1.cs
@@ -0,0 +1,42 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_integral
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e) {
+
+            cartesianChart1.Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title ="Função",
+                    Values = new ChartValues<double>()
+                }
+            };
+            cartesianChart1.AxisX.Add(new Axis
+            {
+                    Title = "X",
+                    Labels = new string[] {}
+            });
+            cartesianChart1.AxisY.Add(new Axis
+            {
+                Title = "f(x)"
+            });
+        }
+    }
+}
